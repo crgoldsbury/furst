7f9414fff051993b8c4c06f9ca8a5ec41772f080
goldzee <goldzee@goldzee-Lenovo-U310>
2019-05-18 13:42:05 -0500

    My vision for the Lexert with comments.

--- include/Lexer.py
+++ include/Lexer.py
@@ -1,15 +1,45 @@
 
 
-class Operators:
-     EQUAL,DOSENTEQUAL,EQUALS,ADD,SUB,DIVIDE,MULITPLY,MOD,SEMICOLON="=","!=","==","+","-","/","*","%",";";
-class types:
-     j_function,j_var,j_string,j_char,j_int,j_float="FUNCTION","VAR","STRING","CHAR","INT","FLOAT";
-class Token:
-    jtype=None;
-    value=None;
-    def __init__(self,jtype,value):
-        self.jtype=jtype
-        self.value=value
-    tok="Token("+jtype+","+value+")";
-    def get(self):
-        return self.tok;
+class Lexer:
+    #CRG:  this isa custom initializer for the class.
+    def __init__(self,codeline):
+        self.codeline = codeline
+        self.listofcodestrings = list()
+        
+    #CRG:  This function will make a decision on what to do with each line.
+    def interpretLine():
+        #CRG: clean up the codeline nad trim the spaces on front or back.
+        #this will lead to greater efficiency.
+        self.codeline = self.codeline.trim()
+                #CRG: if the line is empty we don't want to do anything. 
+        # we can skip it.  This will reduce errors and speed up the
+        # interpretation.
+        
+        if self.codeline.length >0:            
+           #CRG: ok...now split the codeline into a character array.
+           # this will make it easier to iterate through each character.
+           # spaces will be delimiters and tell us  
+           self.listofcodestrings = self.codeline.split()
+           #CRG:  Ok..now we need to iterate through each string
+           # in the list and determine what it is.  It could be:
+           # 1. A value.
+           # 2. A keyword.
+           # 3. An opreator
+           # 4. a variable.
+           # We'll have to pass the string to each function below and determine
+           # which one it is.
+        
+        
+    def IsAValue(self):
+        #CRG: function should return true if it's a user defined value.
+    
+    def IsAKeyword(self):
+        #CRG: function should return true if it's a keyword.
+        
+    
+    def IsAnOperator(self):
+        #CRG: funciton should return true if it's an operator.
+    
+    def IsAVariable(self):
+        #CRG:  function should return true if it's a variable.
+    
--- setup.py
+++ setup.py
@@ -2,12 +2,34 @@
 from include.MAINLIB import *
 from sys import *
 import io
-if __name__ == '__main__':
-    args = argv[1]
-    file = io.open(args)
-    p = FurstInterpreter(file.read())
-    p.start()
-    file.close()
+import Lexer
+
+#CRG: Prior to opening the file; we'll create a Lexer.  We'll need it later.
+decisionmaker = Lexer()
+
+#CRG: First Step...open the source file and read each line.  
+# We do line for line read to be efficient with memory.  
+# Some .js files can be very large and using something like 
+# contents = file.read()  would result in high memory utilization and slow
+# performance.
+
+
+file = open("source.js")
+for line in file:
+    #For each line in the file we want to pass it to a class 
+    #that will decide what to do with the line.
+    Lexer(line)
+
+#CRG: Can't forget to close the file.
+file.close()
+
+#CRG: This is owen's original code.  I've kept, but commented out.
+#if __name__ == '__main__':
+ #   args = argv[1]
+  #  file = io.open(args)
+   # p = FurstInterpreter(file.read())
+    #p.start()
+    #file.close()
 
 
 
